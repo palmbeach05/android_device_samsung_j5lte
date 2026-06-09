92dc56ac22c4e1c8b63ab07896685777b1df04cc
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-08 22:21:30 -0400

    j5lte: Move system_prop values to j5-common
    The values that were in system_prop.mk are common amongst all of the
    j5 2015 devices. It would be more appropriate to have them moved to
    j5-common.

--- device.mk
+++ device.mk
@@ -6,8 +6,5 @@ $(call inherit-product, device/samsung/j5-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/j5lte
 
-# System properties
--include $(LOCAL_PATH)/system_prop.mk
-
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
